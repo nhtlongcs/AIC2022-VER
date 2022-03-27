df990e6b465099be2b088844971b2ca543a59e26
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-03-27 16:09:52 +0700

    update test

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+from pytorch_lightning import seed_everything
+
+TEST_ROOT = os.path.realpath(os.path.dirname(__file__))
+PACKAGE_ROOT = os.path.dirname(TEST_ROOT)
+DATASETS_PATH = os.path.join(PACKAGE_ROOT, "data")
+# generate a list of random seeds for each test
+ROOT_SEED = 1234
+
+
+def reset_seed():
+    seed_everything()
--- tests/parser.py
+++
@@ -1,7 +0,0 @@
-# python tests/parser.py -c configs/template.yml -o global.debug=False
-from opt import Opts
-
-if __name__ == "__main__":
-    opts = Opts().parse_args()
-    print(opts)
-    print("Test case passed")
