63839dac770973d59b25483c9d19218d81046ce5
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-04-02 00:54:55 +0700

    replace data test path

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,12 +5,9 @@ from .helper import create_fake_data
 
 TEST_ROOT = os.path.realpath(os.path.dirname(__file__))
 PACKAGE_ROOT = os.path.dirname(TEST_ROOT)
-DATASETS_PATH = os.path.join(PACKAGE_ROOT, "data")
+DATASETS_PATH = os.path.join(PACKAGE_ROOT, "data_sample")
 # generate a list of random seeds for each test
 ROOT_SEED = 1234
-
-if not (os.path.exists(os.path.join(DATASETS_PATH, "meta"))):
-    create_fake_data()
 
 
 def reset_seed():
