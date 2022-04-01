6165c998a42f8a3db36fc7960e37fba12a15ec1b
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-04-02 02:24:28 +0700

    add error logging of test commands

--- tests/data/test_preproc.py
+++ tests/data/test_preproc.py
@@ -1,29 +1,50 @@
 import pytest
 import os
+
 from tests import DATASETS_PATH
 import os.path as osp
+import subprocess
+
+
+def run(cmd: str):
+
+    process = subprocess.Popen(
+        cmd.split(), stderr=subprocess.PIPE, stdout=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+    exit_code = process.wait()
+    return exit_code, stderr
 
 
 def gen_motion_map(DATAPATH):
     command = f"python scripts/data/motion_map.py {DATAPATH}"
-    assert os.system(command) == 0, "Run test motion map failed, please check"
+    status, output = run(command)
+    assert status == 0, f"{output}\nRun test motion map failed, please check"
 
 
 def gen_nlpaug(DATAPATH):
     command = f"python scripts/data/nlpaug_uts.py {DATAPATH}/train_tracks.json"
-    assert os.system(command) == 0, "Run test augment nlp failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test augment nlp failed, please check"
     command = f"python scripts/data/nlpaug_uts.py {DATAPATH}/test_queries.json"
-    assert os.system(command) == 0, "Run test augment nlp failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test augment nlp failed, please check"
 
 
 def split(DATAPATH):
     command = f"python scripts/data/split.py {DATAPATH}/train_tracks.json"
-    assert os.system(command) == 0, "Run test split failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test split failed, please check"
 
 
 def extract_srl(DATAPATH):
     command = f"python scripts/srl/extraction.py {DATAPATH} {DATAPATH}"
-    assert os.system(command) == 0, "Run test extract srl failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test extract srl failed, please check"
 
 
 def extract_srl_prep(DATAPATH):
@@ -34,11 +55,17 @@ def extract_srl_prep(DATAPATH):
     os.makedirs(f"{SRL_PATH}/veh", exist_ok=True)
 
     command = f"python scripts/srl/action_prep.py {SRL_PATH} {SRL_PATH}/action"
-    assert os.system(command) == 0, "Run test extract srl failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test extract srl failed, please check"
     command = f"python scripts/srl/color_prep.py {DATAPATH} {SRL_PATH} {DATAPATH}/extracted_frames {SRL_PATH}/color"
-    assert os.system(command) == 0, "Run test extract srl failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test extract srl failed, please check"
     command = f"python scripts/srl/veh_prep.py {DATAPATH} {SRL_PATH} {DATAPATH}/extracted_frames {SRL_PATH}/veh"
-    assert os.system(command) == 0, "Run test extract srl failed, please check"
+    status, output = run(command)
+
+    assert status == 0, f"{output}\nRun test extract srl failed, please check"
 
 
 @pytest.mark.first
